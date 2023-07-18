769a3a591f94c6700a8974551cf708d625dcd0b8
everblazingjay <everblazingjay@gmail.com>
2023-07-19 00:19:53 +0100

    first commit on this project

---
+++ datatypes.js
@@ -0,0 +1,42 @@
+// 5
+let str = "this is a string";
+let boolin = true;
+let repo;
+let empty = null;
+let numba = 10;
+
+console.log(typeof str);
+console.log(typeof boolin);
+console.log(typeof repo);
+console.log(typeof empty);
+console.log(typeof numba);
+
+// 6
+
+let me;
+let you;
+let them;
+let everyone;
+
+// 7
+let score = 10;
+let winner = "Madrid";
+let scorer = "Ronaldo";
+let gameOver = true;
+
+// 8
+
+let firstName = "Jay";
+let lastNmae = "Micky";
+let status = "Single";
+let country = "Nigeria";
+let age = 20;
+
+// 9
+let fullName = "Jay Micky a 20years old Single Nigeria";
+
+// 10
+let myAge = "I am 21 years old";
+let yourAge = "You are 15 years old";
+
+console.log(myAge, yourAge);
---
+++ variable.js
@@ -0,0 +1,11 @@
+//Comments can make code readable
+
+//Welcome to 30DaysOfJavascript
+
+/* comments can make code readable,
+easy to reuse and informative */
+
+let str = "this is a string";
+let boolin = true;
+repo = 23;
+let empty = null;
